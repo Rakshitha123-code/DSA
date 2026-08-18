fb03ad52a8b472bde38d20d547c0e35c4ba30b26
Rakshitha <r06063629@gmail.com>
2026-08-18 23:13:39 +0530

    Maximum in Array

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Arrays{
+/*class Arrays{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int sum=0;
@@ -7,5 +7,22 @@ class Arrays{
             sum+=arr[i];
         }
         System.out.println("Sum of array elements: " + sum);
+    }
+}*/
+class Arrays{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int max=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Maximum element in the array: " + max);
     }
 }
