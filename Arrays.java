4330e3ac717467bea9e7bc6aed2499f2a49716e2
Rakshitha <r06063629@gmail.com>
2026-08-18 22:57:29 +0530

    Add DSA practice

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Arrays{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=i;
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
