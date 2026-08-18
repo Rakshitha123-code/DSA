2e306e244a7c006b7042be38388d28ef6fbe6f4f
Rakshitha <r06063629@gmail.com>
2026-08-18 23:04:58 +0530

    Add array elements sum

--- Arrays.java
+++ Arrays.java
@@ -3,8 +3,8 @@ class Arrays{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int sum=0;
-        for (int i = 0; i < arr.length; i++) {
-            sum+=i;
+        for (int i = 0; i <= arr.length-1; i++) {
+            sum+=arr[i];
         }
         System.out.println("Sum of array elements: " + sum);
     }
